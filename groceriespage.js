b5b52a5090162d37e8412c4289aea265ff5d6053
Surajy001 <soorajy001@gmail.com>
2022-07-23 07:45:37 +0530

    GroceriesPage

---
+++ groceriespage.js
@@ -0,0 +1,185 @@
+
+
+let products=[
+    {
+        name:"Good Life Sugar (M) 1 kg",
+        price:"₹ 46.00",
+        image:"https://www.jiomart.com/images/product/150x150/491551493/good-life-sugar-m-1-kg-product-images-o491551493-p491551493-0-202204281542.jpg",
+        logo1:"",
+        logo2:"",
+        uqid:"0001",
+    } ,
+    
+    {
+        name:"Sunfeast Dark Fantasy Choco Fills Cookies 75 g",
+        price:"₹ 37.00",
+        image:"https://www.jiomart.com/images/product/150x150/490762566/sunfeast-dark-fantasy-choco-fills-cookies-75-g-product-images-o490762566-p490762566-0-202206071739.jpg",
+        logo1:"",
+        logo2:"",
+    },  
+    {
+        name:"Dhruv Assorted Beads and Charm Premium Rakhi",
+        price:"₹ 24.00  ", 
+        image:"https://www.jiomart.com/images/product/600x600/490851484/dhruv-assorted-beads-and-charm-premium-rakhi-product-images-o490851484-p590365770-0-202207191830.jpg",
+        logo1:"",
+        logo2:"",
+    },  
+    {
+        name:"Dhruv Pearl With Charm Heavy Design Rakhi",
+        price:"₹ 19.00",
+        image:" https://www.jiomart.com/images/product/600x600/490566456/dhruv-pearl-with-charm-heavy-design-rakhi-product-images-o490566456-p590365769-0-202207191829.jpg ",
+        logo1:"",
+        logo2:"",
+    },  
+    {
+        name:"Dhruv Assorted Beads Medium Rakhi",
+        price:"₹ 14.00",
+        image:"https://www.jiomart.com/images/product/600x600/490566455/dhruv-assorted-beads-medium-rakhi-product-images-o490566455-p590365768-0-202207191830.jpg",
+        logo1:"",
+        logo2:"",
+    },  
+    {
+        name:"Dhruv Assorted Beads Sleek Design Rakhi",
+        price:"₹ 9.00",
+        image:"https://www.jiomart.com/images/product/600x600/490566454/dhruv-assorted-beads-sleek-design-rakhi-product-images-o490566454-p590365767-0-202207191829.jpg",
+        logo1:"",
+        logo2:"",
+    },  
+    {
+        name:"Saffola Gold Refined Rice Bran And Sunflower Based Blended Oil 3 L +",
+        price:"₹ 915.00",
+        image:"https://www.jiomart.com/images/product/600x600/592200116/saffola-gold-refined-rice-bran-and-sunflower-based-blended-oil-3-l-daawat-devaaya-basmati-rice-5-kg-combo-pack-0-20220624.jpg",
+        logo1:"",
+        logo2:"",
+    },  
+
+    {
+        name:"Daawat Devaaya Basmati Rice 1 kg",
+        price:"₹ 92.00",
+        image:"https://www.jiomart.com/images/product/600x600/490005639/daawat-devaaya-basmati-rice-1-kg-product-images-o490005639-p490005639-0-202205172326.jpg",
+        logo1:"",
+        logo2:"",
+    },  
+
+    {
+        name:"Daawat Rozana Super Basmati Rice 1 kg",
+        price:"₹ 70.00",
+        image:"https://www.jiomart.com/images/product/600x600/490863714/daawat-rozana-super-basmati-rice-1-kg-product-images-o490863714-p490863714-0-202203171018.jpg",
+        logo1:"",
+        logo2:"",
+    },  
+
+    {
+        name:"Elina Long Grain Rice 1 kg",
+        price:"₹ 50.00",
+        image:"https://www.jiomart.com/images/product/600x600/491173885/elina-long-grain-rice-1-kg-product-images-o491173885-p491173885-0-202203151443.jpg",
+        logo1:"",
+        logo2:"",
+    },  
+
+    {
+        name:"Daawat Pulav Basmati Rice 1 kg",
+        price:"₹ 111.00",
+        image:"https://www.jiomart.com/images/product/600x600/490863678/daawat-pulav-basmati-rice-1-kg-product-images-o490863678-p490863678-0-202205172225.jpg",
+        logo1:"",
+        logo2:"",
+    },  
+
+    {
+        name:"Daawat Super Basmati Rice 1 kg",
+        price:"₹ 120.00",
+        image:"https://www.jiomart.com/images/product/600x600/490000003/daawat-super-basmati-rice-1-kg-product-images-o490000003-p490000003-0-202203151745.jpg",
+        logo1:"",
+        logo2:"",
+    }, 
+    
+    {
+        name:"Daawat Biryani Basmati Rice 1 kg",
+        price:"₹ 179.00",
+        image:"https://www.jiomart.com/images/product/600x600/490863667/daawat-biryani-basmati-rice-1-kg-product-images-o490863667-p490863667-0-202205191716.jpg",
+        logo1:"",
+        logo2:"",
+    },
+
+     
+    {
+        name:"Daawat Brown Basmati Rice 1 kg",
+        price:"₹ 145.00",
+        image:"https://www.jiomart.com/images/product/600x600/490419418/daawat-brown-basmati-rice-1-kg-product-images-o490419418-p490419418-0-202203150830.jpg",
+        logo1:"",
+        logo2:"",
+    },
+
+    {
+        name:"Daawat Devaaya Everyday Rice 5 kg",
+        price:"₹ 262.00",
+        image:"https://www.jiomart.com/images/product/600x600/491638884/daawat-devaaya-everyday-rice-5-kg-product-images-o491638884-p491638884-0-202203151831.jpg",
+        logo1:"",
+        logo2:"",
+    },
+
+    {
+        name:"Daawat Devaaya Biryani Basmati Rice 1 kg",
+        price:"₹ 105.00",
+        image:"https://www.jiomart.com/images/product/600x600/492059077/daawat-devaaya-biryani-basmati-rice-1-kg-product-images-o492059077-p590319450-0-202203170319.jpg",
+        logo1:"",
+        logo2:"",
+    },
+
+    {
+        name:"Daawat Rozana Gold Basmati Rice 5 kg",
+        price:"₹ 399.00",
+        image:"https://www.jiomart.com/images/product/600x600/490005638/daawat-rozana-gold-basmati-rice-5-kg-product-images-o490005638-p490005638-0-202203171005.jpg",
+        logo1:"",
+        logo2:"",
+    },
+
+    {
+        name:"Daawat Dubar Basmati Rice 1 kg",
+        price:"₹ 99.00",
+        image:"https://www.jiomart.com/images/product/600x600/491091893/daawat-dubar-basmati-rice-1-kg-product-images-o491091893-p491091893-0-202205191716.jpg",
+        logo1:"",
+        logo2:"",
+    },
+
+    {
+        name:"Daawat Traditional Basmati Rice 1 kg",
+        price:"₹ 185.00",
+        image:"https://www.jiomart.com/images/product/600x600/490005499/daawat-traditional-basmati-rice-1-kg-product-images-o490005499-p490005499-0-202203151142.jpg",
+        logo1:"",
+        logo2:"",
+    },
+
+    {
+        name:"Daawat Rozana Super Basmati Rice 5 kg",
+        price:"₹ 349.00",
+        image:"https://www.jiomart.com/images/product/600x600/491019524/daawat-rozana-super-basmati-rice-5-kg-product-images-o491019524-p491019524-0-202203171025.jpg",
+        logo1:"",
+        logo2:"",
+    },
+
+
+];
+
+
+products.map(function (elem){
+    let div=document.createElement("div");
+
+    let image=document.createElement("img");
+    image.setAttribute("src",elem.image)
+
+    let proName=document.createElement("h5");
+    proName.innerText=elem.name;
+
+    let proPrice=document.createElement("h4");
+    proPrice.innerText=elem.price;
+
+    let button=document.createElement("button")
+    button.innerText="Add to cart +";
+
+    div.append(image,proName,proPrice,button)
+
+    document.querySelector("#right-bottom2").append(div);
+});
+
+
