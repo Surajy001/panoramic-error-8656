811d61fa9641f4816db33d6fa9c87b719475c7a3
Surajy001 <soorajy001@gmail.com>
2022-07-24 08:01:53 +0530

    day-5

---
+++ premiumfruits.js
@@ -0,0 +1,254 @@
+
+
+
+
+    let products=[
+        {
+         name:"Dragonfruit Red Flesh Premium Indian 1 Pc (Approx 300 g - 450 g)",
+         price:"199.00",
+         image:"https://www.jiomart.com/images/product/600x600/590860310/dragonfruit-red-flesh-premium-indian-1-pc-approx-300-g-450-g-0-20211112.jpg",
+         logo1:"",
+         logo2:"",
+         uqId:"00001",
+        },
+     
+       {
+         name:"Mangosteen Premium Indian 6 Pc (Approx 490 g - 570 g)",
+         price:"399.00",
+         image:"https://www.jiomart.com/images/product/600x600/591238248/mangosteen-premium-indian-6-pc-approx-490-g-570-g-0-20220506.jpg",
+         logo1:"",
+         logo2:"",
+       },  
+
+       {
+         name:"Rambutan Premium Indian 6 Pc (Approx 150 g - 200 g)",
+         price:"149.00",
+         image:"https://www.jiomart.com/images/product/600x600/591189431/rambutan-premium-indian-6-pc-approx-190-g-230-g-0-20220303.jpg",
+         logo1:"",
+         logo2:"",
+       }, 
+
+       {
+         name:"Plum Premium Indian Pack 500 g",
+         price:"149.00",
+         image:"https://www.jiomart.com/images/product/600x600/591238255/plum-premium-indian-pack-500-g-0-20220506.jpg",
+         logo1:"",
+         logo2:"",
+       }, 
+
+       {
+         name:"Sweet Tamarind Premium Imported Pack 250 g",
+         price:"249.00",
+         image:"https://www.jiomart.com/images/product/600x600/590968389/sweet-tamarind-premium-imported-pack-250-g-0-20211230.jpg",
+         logo1:"",
+         logo2:"",
+       }, 
+
+       {
+         name:"Cherry Red Premium Imported Pack 250 g",
+         price:"489.00",
+         image:"https://www.jiomart.com/images/product/600x600/591189411/cherry-red-premium-imported-pack-250-g-0-20220303.jpg",
+         logo1:"",
+         logo2:"",
+       }, 
+
+       {
+         name:"Peach Sun Gold Premium Indian Pack 500 g",
+         price:"199.00",
+         image:"https://www.jiomart.com/images/product/600x600/591429026/peach-sun-gold-premium-indian-500-g-0-20220518.jpg",
+         logo1:"",
+         logo2:"",
+       }, 
+
+       {
+         name:"Fresh Dates Yellow Premium Indian Pack 500 g",
+         price:"139.00",
+         image:"https://www.jiomart.com/images/product/150x150/591043451/fresh-dates-yellow-premium-indian-pack-500-g-0-20220216.jpg",
+         logo1:"",
+         logo2:"",
+       }, 
+
+       {
+         name:"Custard Apple Premium Indian 2 Pc (Approx 400 g - 600 g)",
+         price:"179.00",
+         image:"https://www.jiomart.com/images/product/600x600/591980101/custard-apple-premium-indian-2-pc-approx-750-g-950-g-0-20220607.jpg",
+         logo1:"",
+         logo2:"",
+       }, 
+
+       {
+         name:"Pineapple Queen Jumbo Premium Indian 1 Pc (Approx 1.2 kg - 1.5 kg)",
+         price:"149.00",
+         image:"https://www.jiomart.com/images/product/600x600/590860309/pineapple-queen-jumbo-premium-indian-1-pc-approx-1-4-kg-1-8-kg-0-20211116.jpg",
+         logo1:"",
+         logo2:"",
+       }, 
+
+       {
+         name:"Papaya Mini Premium Indian 1 Pc (Approx 400 g - 600 g)",
+         price:"49.00",
+         image:"https://www.jiomart.com/images/product/600x600/590860305/papaya-mini-premium-indian-1-pc-approx-400-g-600-g-0-20211112.jpg",
+         logo1:"",
+         logo2:"",
+       }, 
+
+       {
+         name:"Coconut Tender Pollachi Premium Indian 1 Pc (Approx 1 Kg - 1.5 Kg)",
+         price:"59.00",
+         image:"https://www.jiomart.com/images/product/600x600/590860301/coconut-tender-pollachi-premium-indian-1-pc-approx-1-kg-1-5-kg-0-20211118.jpg",
+         logo1:"",
+         logo2:"",
+       }, 
+
+       {
+         name:"Mango Langda Premium Indian 3 Pc (Approx 0.90 kg - 1.20 kg)",
+         price:"199.00",
+         image:"https://www.jiomart.com/images/product/600x600/591238247/mango-langda-premium-indian-4-pc-approx-0-90-kg-1-20-kg-0-20220506.jpg",
+         logo1:"",
+         logo2:"",
+       }, 
+
+       {
+         name:"Coconut Madras Jumbo Premium Indian 1 Pc (Approx 700 g - 900 g)",
+         price:" 69.00",
+         image:"https://www.jiomart.com/images/product/600x600/590860295/coconut-madras-jumbo-premium-indian-1-pc-approx-700-g-900-g-0-20211116.jpg",
+         logo1:"",
+         logo2:"",
+       }, 
+
+       {
+         name:"Banana Red Premium Indian 3 Pc (Approx 405 g - 495 g)",
+         price:"99.00",
+         image:"https://www.jiomart.com/images/product/600x600/590860290/banana-red-premium-indian-3-pc-approx-405-g-495-g-0-20211116.jpg",
+         logo1:"",
+         logo2:"",
+       }, 
+
+       {
+         name:"Banana Nendran Premium Indian 2 Pc (Approx 350 g - 400 g)",
+         price:"89.00",
+         image:"https://www.jiomart.com/images/product/600x600/590860289/banana-nendran-premium-indian-2-pc-approx-350-g-400-g-0-20211117.jpg",
+         logo1:"",
+         logo2:"",
+       }, 
+
+       {
+         name:"Mangosteen Premium Indian 6 Pc (Approx 490 g - 570 g)",
+         price:"399.00",
+         image:"https://www.jiomart.com/images/product/600x600/591238248/mangosteen-premium-indian-6-pc-approx-490-g-570-g-0-20220506.jpg",
+         logo1:"",
+         logo2:"",
+       }, 
+
+       {
+         name:"Pear Babugosha Premium Indian 4 Pc (Approx 0.90 kg - 1.10 kg)",
+         price:"149.00",
+         image:"https://www.jiomart.com/images/product/600x600/592292185/pear-babugosha-premium-indian-4-pc-approx-0-90-kg-1-10-kg-0-20220630.jpg",
+         logo1:"",
+         logo2:"",
+       }, 
+
+       {
+         name:"Apple Dazzle Premium Imported 1 pc (Approx 175 g - 200 g)",
+         price:"80.00",
+         image:"https://www.jiomart.com/images/product/600x600/591193903/apple-dazzle-premium-imported-1-pc-approx-175-g-200-g-0-20220309.jpg",
+         logo1:"",
+         logo2:"",
+       }, 
+
+       {
+         name:"Dates Oriental King Solomon Premium Imported Pack 5 Kg",
+         price:"2290.00",
+         image:"https://www.jiomart.com/images/product/600x600/591021537/dates-oriental-premium-imported-pack-5-kg-0-20220127.jpg",
+         logo1:"",
+         logo2:"",
+       }, 
+
+ ];
+
+
+
+
+ let tempProducts = products.slice();
+
+
+ let AddCartItemArr=JSON.parse(localStorage.getItem("Add-Cart")) || [];
+
+
+ document.querySelector("#High-to-Low").addEventListener("click",Byprice);
+ document.querySelector("#Low-to-High").addEventListener("click",Byprice2);
+ document.querySelector("#All-Products").addEventListener("click",Byprice3);
+
+
+
+    
+ function displayData (data) {
+     document.querySelector("#right-bottom20").innerHTML = "";
+            
+     data.map(function (elem){
+       let div=document.createElement("div");
+   
+       let image=document.createElement("img");
+       image.setAttribute("src",elem.image)
+   
+       let proName=document.createElement("h5");
+       proName.innerText=elem.name;
+   
+       let proPrice=document.createElement("h4");
+       proPrice.innerText= "₹"+ elem.price;
+   
+       let button=document.createElement("button")
+       button.innerText="Add to cart +";
+       button.addEventListener("click",function(){
+           AddCartItem(elem)
+           
+       })
+   
+       div.append(image,proName,proPrice,button)
+     //   document.querySelector("#right-bottom2").append(div);
+       document.querySelector("#right-bottom20").append(div);
+   });
+       }
+
+
+       
+ function AddCartItem(elem){
+     console.log(AddCartItemArr)
+     AddCartItemArr.push(elem);
+     localStorage.setItem("Add-Cart",JSON.stringify(AddCartItemArr));
+ }
+
+
+ function Byprice(){
+     let selectprice=document.querySelector("#High-to-Low").value    
+     let sortedProducts = products;
+     if(selectprice=="High"){
+         sortedProducts.sort(function (a,b){
+         return Number(b.price)-Number(a.price)
+     });
+  
+ }
+ //console.log(products);
+ displayData(sortedProducts);
+
+
+}
+function Byprice2(){
+ let selectprice=document.querySelector("#Low-to-High").value    
+ let sortedProducts = products;
+ if(selectprice=="Low"){
+     sortedProducts.sort(function (a,b){
+     return Number(a.price)-Number(b.price)
+ });
+ 
+ }
+ //console.log(sortedProducts);
+ displayData(sortedProducts);
+
+
+ }
+ function Byprice3(){
+     displayData(tempProducts);  
+ }
+
+ displayData(products);
